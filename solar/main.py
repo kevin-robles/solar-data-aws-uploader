dffd1194b6092266399438362545fde78f365c76
= <=>
2023-02-25 11:22:12 -0400

    Calling the websiteConnection class to generate the file. Also added serial number to the secrets file to be able to pass it as a parameter.

--- solar/main.py
+++ solar/main.py
@@ -2,7 +2,8 @@ import os
 import json
 import pathlib as pl
 from authentication import Authentication
-
+from website_connection import WebsiteConnection
+from datetime import datetime, timedelta
 
 class Main:
     def __init__(self):
@@ -12,11 +13,16 @@ class Main:
             secrets = json.load(f)
         self.username = secrets['PV_USERNAME']
         self.password = secrets['PV_PASSWORD']
+        self.serial_number = secrets['PV_SERIAL_NUMBER']
         self.auth = Authentication(self.username, self.password)
 
     def run(self):
         self.auth.get_access_token()
-        print(self.auth.access_token)
+        access_token = self.auth.access_token
+        today = datetime.now().strftime('%Y-%m-%d')
+        yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        file_generator = WebsiteConnection(access_token, self.username, self.password, today, yesterday)
+        file = file_generator.get_file()
 
 
 if __name__ == "__main__":
